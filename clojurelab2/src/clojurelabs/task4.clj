81f2bec61eaf38eaaf551c0e9c972c27a57bd9bf
Irina Fediaeva <irina.fediaeva@jetbrains.com>
2020-10-14 22:25:40 +0700

    Clojure lab 1.4 with collections

--- clojurelab2/src/clojurelabs/task4.clj
+++ clojurelab2/src/clojurelabs/task4.clj
@@ -1,10 +1,10 @@
 (ns clojurelabs.task4)
 
 (defn exclude-repeated [symbol items]
-  (filter (fn [it] (not= symbol (subs it 0 1))) items))
+  (filter (fn [it] (not= symbol (first it))) items))
 
 (defn increment-one [symbol items]
-  (map (fn [coll] (str symbol coll)) (exclude-repeated symbol items)))
+  (map (fn [coll] (conj coll symbol)) (exclude-repeated symbol items)))
 
 (defn increment-one-to-all [alphabet items]
   (map (fn [symbol] (increment-one symbol items)) alphabet))
@@ -19,13 +19,19 @@
     (recur (increment-symbols alphabet items) (dec n) alphabet)
     ))
 
+(defn init-alph [alph]
+  (map (fn [symbol] (list symbol)) alph))
+
+(defn to-strings [items]
+  (map (fn [it] (apply str it)) items))
+
 (defn generate [alphabet n]
   (if (> n 0)
-    (recur-n-times alphabet (dec n) alphabet)
+    (to-strings (recur-n-times (init-alph alphabet) (dec n) alphabet))
     '()))
 
 (defn -main [& args]
-  (println (generate '("a" "b" "c" "d") 5))
-  (println (generate '("a" "b" "s") 4))
+  (println (generate '("a" (:b 1) ['c 'd]) 3))
+  (println (generate '("a" "b" "c" "d") 3))
   )
 

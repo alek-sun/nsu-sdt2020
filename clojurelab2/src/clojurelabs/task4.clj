fc73c42ff2d8b47a14315070870a5cf280c71703
Irina Fediaeva <irina.fediaeva@jetbrains.com>
2020-10-15 16:32:54 +0700

    Clojure lab 1.4 with iterate

--- clojurelab2/src/clojurelabs/task4.clj
+++ clojurelab2/src/clojurelabs/task4.clj
@@ -6,32 +6,18 @@
 (defn increment-one [symbol items]
   (map (fn [coll] (conj coll symbol)) (exclude-repeated symbol items)))
 
-(defn increment-one-to-all [alphabet items]
-  (map (fn [symbol] (increment-one symbol items)) alphabet))
-
 (defn increment-symbols [alphabet items]
-  (reduce concat `() (increment-one-to-all alphabet items)))
+  (mapcat (fn [symbol] (increment-one symbol items)) alphabet))
 
-(defn recur-n-times
-  [items n alphabet]
-  (if (= n 0)
-    items
-    (recur (increment-symbols alphabet items) (dec n) alphabet)
-    ))
-
-(defn init-alph [alph]
-  (map (fn [symbol] (list symbol)) alph))
+(defn iterate-n [n alphabet]
+  (nth (iterate (increment-symbols alphabet '(())) '(())) n))
 
 (defn to-strings [items]
   (map (fn [it] (apply str it)) items))
 
-(defn generate [alphabet n]
-  (if (> n 0)
-    (to-strings (recur-n-times (init-alph alphabet) (dec n) alphabet))
-    '()))
-
 (defn -main [& args]
-  (println (generate '("a" (:b 1) ['c 'd]) 3))
-  (println (generate '("a" "b" "c" "d") 3))
+  ;(println (generate '("a" (:b 1) ['c 'd]) 3))
+  ;(println (generate '("a" "b" "c" "d") 3))
+  (println (iterate-n 3 '("a" "b" "c" "d")))
   )
 

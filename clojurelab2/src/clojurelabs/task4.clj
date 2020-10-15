55dfc6dae16f8c5c8b504cb6ef43d452490785bf
Irina Fediaeva <irina.fediaeva@jetbrains.com>
2020-10-15 17:01:38 +0700

    Clojure lab 1.4 fxd

--- clojurelab2/src/clojurelabs/task4.clj
+++ clojurelab2/src/clojurelabs/task4.clj
@@ -10,14 +10,14 @@
   (mapcat (fn [symbol] (increment-one symbol items)) alphabet))
 
 (defn iterate-n [n alphabet]
-  (nth (iterate (increment-symbols alphabet '(())) '(())) n))
+  (nth (iterate (partial increment-symbols alphabet) '(())) n))
 
 (defn to-strings [items]
   (map (fn [it] (apply str it)) items))
 
 (defn -main [& args]
-  ;(println (generate '("a" (:b 1) ['c 'd]) 3))
-  ;(println (generate '("a" "b" "c" "d") 3))
   (println (iterate-n 3 '("a" "b" "c" "d")))
+  (println (iterate-n 3 '("a" (:b 1) ['c 'd])))
+  (println (iterate-n 5 '("a" "b" "c" "d")))
   )
 

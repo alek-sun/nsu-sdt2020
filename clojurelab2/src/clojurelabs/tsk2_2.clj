303dd45d37cbfc0e5ea70fc869307352fb99a8b5
Irina Fediaeva <irina.fediaeva@jetbrains.com>
2020-10-24 14:09:26 +0700

    Clojure lab 2.2 fix

--- clojurelab2/src/clojurelabs/tsk2_2.clj
+++ clojurelab2/src/clojurelabs/tsk2_2.clj
@@ -6,12 +6,10 @@
   (* (- x2 x1) (/ (+ (f x1) (f x2)) 2.0))
   )
 
-(defn integrator [f step]
+(defn integrate [f step]
   (let [steps (map (partial * step) (range))
-        sequent (reductions
-                  +
-                  0
-                  (map (partial trapeze-area f) steps (rest steps)))]
+        sequent (reductions + 0
+                            (map (partial trapeze-area f) steps (rest steps)))]
     (fn [b]
       (let [r (mod b step)
             n-step (int (/ (- b r) step))]
@@ -26,26 +24,23 @@
     )
   )
 
-(defn integrate [f step b]
-  (let [init-coll (integrator f step)]
-    (init-coll b)
-    )
-  )
-
 (defn little-diff [real expected]
   (< (Math/abs ^float (- real expected)) 0.0000001)
   )
 
 (defn -main
   []
-  (let [step 0.05]
-    (time (integrate (fn [x] x) step 200))
-    (time (integrate (fn [x] x) step 200))
-    (time (integrate (fn [x] x) step 200))
-    (time (integrate (fn [x] x) step 199))
-    (time (integrate (fn [x] x) step 1.5))
-    (time (integrate (fn [x] x) step 3.7))
-    (println (integrate (fn [x] x) step 2))
-    (println (integrate (fn [x] x) 7 9))
+  (let [step 0.05
+        integrator (integrate (fn [x] x) step)]
+    (time (integrator 200))
+    (time (integrator 201))
+    (time (integrator 199))
+    ;(time (integrate (fn [x] x) step 200))
+    ;(time (integrate (fn [x] x) step 200))
+    ;(time (integrate (fn [x] x) step 199))
+    ;(time (integrate (fn [x] x) step 1.5))
+    ;(time (integrate (fn [x] x) step 3.7))
+    ;(println (integrate (fn [x] x) step 2))
+    ;(println (integrate (fn [x] x) 7 9))
     )
   )
